3f4e0457eaba7f12fda1bd0541290e4436d40afc
Arthl <arthur.lustman@gmail.com>
2019-03-16 10:20:49 +0100

    DBCs completed

--- Functions_Example/ApplyDBCs.m
+++ Functions_Example/ApplyDBCs.m
@@ -4,21 +4,26 @@ function C = ApplyDBCs(X,Example,tol_h)
 % C: two-column matrix with the imposed degreess of freedom in the first
 % column and the corresponding values in the second one. 
 
-dom = Example.dom; 
+dom = Example.dom;
+DBC = Example.DBC;
+
 x1 = dom(1); x2 = dom(2); 
 y1 = dom(3); y2 = dom(4); 
 
-nodes_x1 = find(abs(X(:,1) - x1) < tol_h); 
-nodes_x2 = find(abs(X(:,1) - x2) < tol_h); 
-% nodes_y1 = find(abs(X(:,2) - y1) < tol_h); 
-nodes_y2 = find(abs(X(:,2) - y2) < tol_h); 
+nodes_x1 = find(abs(X(:,1) - x1) < tol_h)*DBC(4); 
+nodes_x2 = find(abs(X(:,1) - x2) < tol_h)*DBC(2); 
+nodes_y1 = find(abs(X(:,2) - y1) < tol_h)*DBC(1); 
+nodes_y2 = find(abs(X(:,2) - y2) < tol_h)*DBC(3); 
 
-% nodesDir = unique([nodes_x1; nodes_x2;nodes_y1; nodes_y2]); 
-nodesDir = unique([nodes_x1; nodes_x2;nodes_y2]); 
+nodesDir = unique([nodes_x1; nodes_x2;nodes_y1; nodes_y2]); 
+% nodesDir = unique([nodes_x1; nodes_x2;nodes_y2]); 
 
+if nodesDir(1) == 0
+    nodesDir = nodesDir(2:end);
+end
 
 u = ExactSol(X(nodesDir,:), Example); 
-nDir = length(nodesDir); 
+% nDir = length(nodesDir); 
 
 C = [nodesDir,u];
 
--- Functions_Example/SetExample.m
+++ Functions_Example/SetExample.m
@@ -8,4 +8,18 @@ example.k = 1;
 example.ID =exID;
 example.dom = [0,1,0,1];
 
+switch example.ID
+    case 1
+        example.DBC = [1,1,1,1];
+        example.NBC = [0,0,0,0];
+    case 3
+        example.DBC = [0,1,0,1];
+        example.NBC = [1,0,1,0];
+        example.ID = 1;
+    case 2
+        example.DBC = [0,1,0,1];
+        example.NBC = [1,0,1,0];
+end
+    
+
 
